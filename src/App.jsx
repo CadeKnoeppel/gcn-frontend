7a77c2edac09963b91cca598a9534583e3824d33
Cade <cdk17@MacBookPro.attlocal.net>
2025-06-19 10:09:06 -0500

    Fix case-sensitive dashboard import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Dashboard from "./pages/Dashboard/dashboard";
+import Dashboard from "./pages/Dashboard/Dashboard";
 import Outreach from "./pages/Outreach/outreach";
 import Tasks from "./pages/Tasks/tasks";
 import Posts from "./pages/Posts/Posts";
