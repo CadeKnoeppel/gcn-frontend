3ccef4fcfd08840263027be7a2a2fa199e69635a
Cade <cdk17@MacBookPro.attlocal.net>
2025-06-19 10:12:38 -0500

    Fix Posts import case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Outreach from "./pages/Outreach/outreach";
 import Tasks from "./pages/Tasks/tasks";
-import Posts from "./pages/Posts/Posts";
+import Posts from "./pages/Posts/posts";
 import Admin from "./pages/Admin/admin";
 import Login from "./pages/Login/Login";
 import Layout from "./components/Layout/Layout";
