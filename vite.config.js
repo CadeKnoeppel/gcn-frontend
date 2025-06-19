8c2c92ef5f4bda08269b56c7ec887c3cbf84f153
Cade <cdk17@MacBookPro.attlocal.net>
2025-06-19 11:16:56 -0500

    Fresh deploy with all pages included

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // 👈 this is key for Vercel static deploys
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
